3f19b3d495fbe994b3cb7d64c9b899bbf3880bf7
Nikolay Shebanov <nikolay.shebanov@gmail.com>
2012-12-23 03:08:44 +0400

    Fix install_generator behavior

--- lib/generators/spree_sunspot_search/install/install_generator.rb
+++ lib/generators/spree_sunspot_search/install/install_generator.rb
@@ -4,7 +4,12 @@ module SpreeSunspotSearch
       source_root File.expand_path('../../../templates/', __FILE__)
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/solr_sort_by\n"
+        default_js = 'app/assets/javascripts/store/all.js'
+        if File.exist?(default_js)
+          append_file default_js, "//= require store/solr_sort_by\n"
+        else
+          puts "#{default_js} does not exist, skipping"
+        end
       end
 
       def copy_initializer_file
